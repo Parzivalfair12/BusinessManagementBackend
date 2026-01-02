8353810632981170411d108bb1e5fa3265fa5722
parzival <andreschaveso@gmail.com>
2026-01-02 15:59:03 -0500

    Feat: Se agrega funcionalidad de login

--- src/serve.ts
+++ src/serve.ts
@@ -1,7 +1,13 @@
 import express from 'express';
 import authRoutes from './infrastructure/http/routes/auth.routes';
+import cors from 'cors';
 
 export const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 
 app.use(express.json());
 app.use('/auth', authRoutes);
